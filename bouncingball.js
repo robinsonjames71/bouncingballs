509c9d2940c3ce6b43e5ad664c5af4f37e18addf
James Robinson <jamesr@blackmagicdesign.com>
2016-05-13 11:36:55 +1000

    Add collision physics

--- bouncingball.js
+++ bouncingball.js
@@ -7,7 +7,8 @@
 		pullStrength = 0.001,
 		offset = 200,
 		circles = [],
-		numCircles = 10;
+		numCircles = 10,
+		repulsion = 1;
 
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -25,7 +26,7 @@
 	}
 
 	function executeFrame(){
-		var i, circle;
+		var i, j, circle;
 		for(i = 0; i < numCircles; i++) {
 			circle = circles[i];
 		
@@ -60,6 +61,11 @@
 				circle.x = offset + circle.radius;
 				circle.vx = Math.abs(circle.vx);
 			}
+
+			// Collision
+			for (j = i + 1; j < numCircles; j++) {
+				collide(circle, circles[j])
+			}
 	
 			c.beginPath();
 			c.arc(circle.x, circle.y, circle.radius, 0, 2*Math.PI);
@@ -74,6 +80,22 @@
 		c.fillRect(0, 0, canvas.width, canvas.height);
 
 		requestAnimationFrame(executeFrame);
+	}
+
+	function collide(a, b) {
+		var dx = b.x - a.x,
+			dy = b.y - a.y,
+			// Distance between balls
+			d = Math.sqrt(dx*dx + dy*dy),
+			ux = dx / d,
+			uy = dy / d;
+
+		if(d < a.radius + b.radius){
+			a.vx -= ux * repulsion;
+			a.vy -= uy * repulsion;
+			b.vx += ux * repulsion;
+			b.vy += uy * repulsion;
+		}
 	}
 
 	canvas.addEventListener('mousemove', function(event){
