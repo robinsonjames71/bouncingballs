51fe8710d68dd2c46057e7fb3bbb1f88a298a7e4
James Robinson <jamesr@blackmagicdesign.com>
2016-05-13 10:40:12 +1000

    Add gravity and dampening to slow down the bounce

--- bouncingball.js
+++ bouncingball.js
@@ -1,16 +1,35 @@
 (function(){
 
 	var canvas = document.getElementById("ball");
-	c = canvas.getContext('2d');
+	c = canvas.getContext('2d'),
+	gravity = 0.1,
+	dampening = 0.99;
 
 	var circle = {
 		x: 50,
 		y: 50,
+		// (vx, vy) = Velocity vector
+		vx: 0,
+		vy: 0,
 		radius: 20
 	};
 
 	function executeFrame(){
-		circle.y++;
+		// Increment location by velocity
+		circle.x += circle.vx;
+		circle.y += circle.vy;
+
+		// Increment Gravity
+		circle.vy += gravity;
+
+		// Slow it down
+		circle.vy *= dampening;
+		circle.vx *= dampening;
+
+		if(circle.y + circle.radius > canvas.height){
+			circle.y = canvas.height - circle.radius;
+			circle.vy = -Math.abs(circle.vy);
+		}
 
 		c.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -19,9 +38,7 @@
 		c.closePath();
 		c.fill();
 
-		if(circle.y < canvas.height - circle.radius) {
-			requestAnimationFrame(executeFrame);
-		}
+		requestAnimationFrame(executeFrame);
 	}
 
 	// Start animation
